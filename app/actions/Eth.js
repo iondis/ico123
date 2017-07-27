9cff3fe91af9b31d48b4515168bd6ad1a25848d7
Sean Pollock <seanpollock3344@gmail.com>
2017-07-27 19:45:00 -0400

    setting up web3 action class. checking for metamask injected web3

---
+++ app/actions/Eth.js
@@ -0,0 +1,16 @@
+export default class Eth {
+  constructor() {
+    this.web3
+  }
+
+  getWeb3Provider() {
+    return (dispatch) => {
+      window.addEventListener('load', () => {
+        if (typeof web3 !== 'undefined') {
+          console.log('web3', web3)
+          this.web3 = new Web3(web3.currentProvider)
+        }
+      })
+    }
+  }
+}
