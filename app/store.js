24edee8994ef68855c4a4cba2cd144a22a995d49
Sean Pollock <seanpollock3344@gmail.com>
2017-07-27 19:40:25 -0400

    setting up redux store with thunk

---
+++ app/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux'
+import thunk from 'redux-thunk'
+
+import  * as reducers from './reducers/index'
+
+const reducer = combineReducers({ ...reducers })
+
+export default createStore(reducer, applyMiddleware(thunk))
