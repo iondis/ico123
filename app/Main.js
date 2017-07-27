d72fdb2b4852efe0e8e8f21d30737cfc8f012d15
Sean Pollock <seanpollock3344@gmail.com>
2017-07-27 19:47:40 -0400

    wrapping main in redux provider

--- app/Main.js
+++ app/Main.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
 
 export default class Main extends Component {
   render() {
     return (
-      <div>
-        <h1>hello world whats up</h1>
-      </div>
+      <Provider store={store}>
+        <h1> whats up world</h1>
+      </Provider>
     )
   }
 }
