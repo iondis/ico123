0b2b86e7b3784319d8aa9d52b03754462108e1b7
Sean Pollock <seanpollock3344@gmail.com>
2017-07-27 22:05:03 -0400

    saffolding flex layout

--- app/Main.js
+++ app/Main.js
@@ -6,7 +6,25 @@ export default class Main extends Component {
   render() {
     return (
       <Provider store={store}>
-        <h1> whats up world</h1>
+        <div>
+          <div style={{ wdith: '100%', height: 800, display: 'flex' }}>
+            <h3>INVEST</h3>
+            <div style={{ width: 500 }}>
+              <h3>Price</h3>
+              <h3></h3>
+              <h3></h3>
+            </div>
+            <div style={{ width: 500 }}>
+              <h3>Total Supply</h3>
+              <h3></h3>
+              <h3>Sold</h3>
+            </div>
+          </div>
+          <div style={{ wdith: '100%', height: 800 }}>
+            <h3>INFO</h3>
+            <div style={{ widtth: 500}}></div>
+          </div>
+        </div>
       </Provider>
     )
   }
