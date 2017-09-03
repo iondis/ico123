1a7640d77a3e93a153fd00603f10836276af0bf3
Sean Pollock <seanpollock3344@gmail.com>
2017-09-03 19:47:18 -0400

    react entry point

--- app/Main.js
+++ app/Main.js
@@ -7,23 +7,7 @@ export default class Main extends Component {
     return (
       <Provider store={store}>
         <div>
-          <div style={{ wdith: '100%', height: 800, display: 'flex' }}>
-            <h3>INVEST</h3>
-            <div style={{ width: 500 }}>
-              <h3>Price</h3>
-              <h3></h3>
-              <h3></h3>
-            </div>
-            <div style={{ width: 500 }}>
-              <h3>Total Supply</h3>
-              <h3></h3>
-              <h3>Sold</h3>
-            </div>
-          </div>
-          <div style={{ wdith: '100%', height: 800 }}>
-            <h3>INFO</h3>
-            <div style={{ widtth: 500}}></div>
-          </div>
+          <h1>ico'in it</h1>
         </div>
       </Provider>
     )
