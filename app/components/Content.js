d9ff52165fd4e40ad39bac824c205502ecbdd3b7
Sean Pollock <seanpollock3344@gmail.com>
2017-09-03 20:20:21 -0400

    content container

---
+++ app/components/Content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Content extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div style={{ width: '100%', backgroundColor: '#A9A9A9' }}>
+
+      </div>
+    )
+  }
+}
