fbfb65a021a01f70140343f547f2f6f93c42bbb5
Sean Pollock <seanpollock3344@gmail.com>
2017-09-11 10:14:28 -0400

    styling for metamask panel

--- app/components/MetaMask.js
+++ app/components/MetaMask.js
@@ -7,11 +7,35 @@ export default class MetaMask extends Component {
 
   render() {
     return (
-      <div>
-        <div style={{ width: 500, height: 500, backgroundColor: 'white', borderRadius: 5 }}>
+      <div style ={mmContStyle}>
+        <div style={mmPanelStyle}>
           <img src='./app/imgs/metamask_logo.png' />
+          <h1>download Metamask!</h1>
         </div>
       </div>
     )
   }
+}
+
+
+const mmContStyle = {
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  fontFamily: 'roboto',
+  color: '#696969'
+}
+
+const mmPanelStyle = {
+  width: 600,
+  height: 500,
+  backgroundColor: 'white',
+  borderRadius: 5,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
 }
