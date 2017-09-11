6fbdd14f18f84eb86a74cc8da7a0bc962d11fa88
Sean Pollock <seanpollock3344@gmail.com>
2017-09-11 11:03:25 -0400

    adding responsive hover ui features to sidenav

--- app/components/sidenav/House.js
+++ app/components/sidenav/House.js
@@ -3,24 +3,50 @@ import React, { Component } from 'react'
 export default class House extends Component {
   constructor() {
     super()
+    this.state = {
+      houseStyle: {
+        height: 180,
+        margin: 20,
+        borderRadius: 5,
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+      }
+    }
   }
 
   onMouseEnterHandler(e) {
-    console.log('e', e)
+    e.preventDefault()
+    // houseStyle.boxShadow = 'boxShadow: 4 8px 12px 4 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+    this.setState(prevState => {
+      return {
+        houseStyle: {
+          ...prevState.houseStyle,
+          boxShadow: '4px 6px 10px 4px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+        }
+      }
+    })
+  }
+
+  onMouseLeaveHandler(e) {
+    e.preventDefault()
+    this.setState(prevState => {
+      return {
+        houseStyle: {
+          ...prevState.houseStyle,
+          boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+        }
+      }
+    })
   }
 
   render() {
     return (
-      <div style={houseStyle} onMouseEnter={this.onMouseEnterHandler}>
+      <div
+        style={this.state.houseStyle}
+        onMouseEnter={this.onMouseEnterHandler.bind(this)}
+        onMouseLeave={this.onMouseLeaveHandler.bind(this)}
+        >
         <img style={{marginLeft: 40, marginRight: 40, marginTop: 20, marginBottom: 20 }} src='./app/imgs/house.png' />
       </div>
     )
   }
-}
-
-const houseStyle = {
-  height: 180,
-  margin: 20,
-  borderRadius: 5,
-  boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
 }
--- app/components/sidenav/Token.js
+++ app/components/sidenav/Token.js
@@ -3,11 +3,47 @@ import React, { Component } from 'react'
 export default class Token extends Component {
   constructor() {
     super()
+    this.state = {
+      tokenStyle: {
+        margin: 20,
+        borderRadius: 5,
+        height: 180,
+        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+      }
+    }
+  }
+
+  onMouseEnterHandler(e) {
+    e.preventDefault()
+    this.setState(prevState => {
+      return {
+        tokenStyle: {
+          ...prevState.tokenStyle,
+          boxShadow: '4px 6px 10px 4px rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+        }
+      }
+    })
+  }
+
+  onMouseLeaveHandler(e) {
+    e.preventDefault()
+    this.setState(prevState => {
+      return {
+        tokenStyle: {
+          ...prevState.tokenStyle,
+          boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+        }
+      }
+    })
   }
 
   render() {
     return (
-      <div style={{ margin: 20, borderRadius: 5, height: 180, boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'}}>
+      <div
+        style={this.state.tokenStyle}
+        onMouseEnter={this.onMouseEnterHandler.bind(this)}
+        onMouseLeave={this.onMouseLeaveHandler.bind(this)}
+        >
         <img style={{marginLeft: 40, marginRight: 40, marginTop: 20, marginBottom: 20 }} src='./app/imgs/logos/veridium_logo.png' />
       </div>
     )
